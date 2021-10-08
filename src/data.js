762e78eeb872f36f46d2bff7639c062be007b791
BariqPribadi <50965999+BariqPribadi@users.noreply.github.com>
2021-10-08 15:30:39 +0700

    first

---
+++ src/data.js
@@ -0,0 +1,58 @@
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "SANMOL 500MG",
+    img: "img/1.jpg",
+    price: 2000,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "OBH COMBI 100ML",
+    img: "img/2.jpg",
+    price: 13000,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "PROMAG TABLET",
+    img: "img/3.jpg",
+    price: 20000,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "BETADINE ANTISEPTIC SOLUTION 30ML",
+    img: "img/4.jpg",
+    price: 3000,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "BODREX TABLET 20S",
+    img: "img/5.jpg",
+    price: 8000,
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+];
+
+export const detailProduct = {
+    id: 0,
+    title: 0,
+    img: 0,
+    price: 0,
+    inCart: false,
+    count: 0,
+    total: 0
+};
